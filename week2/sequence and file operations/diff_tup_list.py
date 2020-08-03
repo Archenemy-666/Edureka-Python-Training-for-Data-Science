3a9da65e98ccdd489b4499857690ce422bd81d2f
Siddharth <csiddu98@outlook.com>
2020-08-03 08:29:30 +0530

    day3 complete

--- week2/sequence and file operations/diff_tup_list.py
+++ week2/sequence and file operations/diff_tup_list.py
@@ -1,14 +1,22 @@
 #List
-list=[1,2,"Python","Android"]
+list1=[1,2,"Python","Android"]
 print(list)
 
-list[2]="Data Science"
+list1[2]="Data Science"
 print(list)
 
 #Tuple
 tup=(1,2,3,"Data Science")
 print(tup)
-
+print(list1)
+list1[-2::-1]
+list1[-2::][::-1]
+list1[::2]
+list1[-1:-2:]
+h = "Siddharth"
+b = "cool"
+print(b.title())
+print("hello %s world %s"%(h,b))
 tup[1]="Python"
 print(tup)
 
--- week2/sequence and file operations/set_opr.py
+++ week2/sequence and file operations/set_opr.py
@@ -4,7 +4,7 @@ print(X)
 A={1,2,3,4}
 B={3,4,5,6}
 print(A|B)
-print(A&B)
+print(A and B)
 
 A={1,2,3,4,5}
 B={4,5,6,7,8}
