7dc5f1ac00ce01f8acb32d876809d69cbadb7afa
Siddharth <csiddu98@outlook.com>
2020-08-20 12:35:05 +0530

    skipped to data manipulation

--- assignments/numpycase/case4.py
+++ assignments/numpycase/case4.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 
 read = pd.read_csv('BigMartSalesData.csv')
 df = pd.DataFrame(read)
-
+invoice = np.array(df['InvoiceNo'])
+amountdf = np.array(df['Amount'])
+plt.scatter(invoice,amountdf)
 year2011 = (df[df['Year']==2011])
 month = np.array(year2011['Month'])
 amount = np.array(year2011['Amount'])
 country = np.array(year2011['Country'])
-x = plt.plot(month,amount)
-plt.pie(amount, labels = country)
+
+print(plt.plot(month,amount))
+#plt.pie(amount, labels = country)
 month8 = (year2011[year2011['Month'] == 8]) 
 np.mean(month8['Amount'])
 x = np.min(month8['Amount'])
